483f13dae6d2329ac6451f2bf85db90f172a84fc
Joshua Barrass <josh.barrass.work@gmail.com>
2024-12-02 16:14:20 +0000

    Day 1 Part 2

--- Day1/src/Main.hs
+++ Day1/src/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import ParseArgs
 import Data.List.Split
-import Data.List
+import qualified Data.Map as Map
 
 parseLine :: String -> (Integer, Integer)
 parseLine x = let
@@ -12,14 +12,28 @@ parseLine x = let
 parseLines :: [String] -> [(Integer, Integer)]
 parseLines = map parseLine
 
-distance :: Integer -> Integer -> Integer
-distance x y = abs (x - y)
+getCountsMap :: [Integer] -> Map.Map Integer Integer
+getCountsMap [] = Map.empty
+getCountsMap (x:xs)
+  | x `Map.member` rest = Map.update (\y -> Just (y+1)) x rest
+  | otherwise = Map.insert x 1 rest
+  where rest = getCountsMap xs
+
+getCount :: Map.Map Integer Integer -> Integer -> Integer
+getCount m x
+  | x `Map.member` m = m Map.! x
+  | otherwise = 0
+
+sim :: Map.Map Integer Integer -> Integer -> Integer
+sim m x = x * (getCount m x)
 
 main :: IO ()
 main = do
   l <- loadInputFileLines
   let parsed = parseLines l
+  -- let parsed = [(3,4), (4,3), (2,5), (1,3), (3,9), (3,3)]
   let (x, y) = unzip parsed
-  let diffs = zipWith distance (sort x) (sort y)
-  let total = sum diffs
+  let counts = getCountsMap y
+  let sims = map (sim counts) x
+  let total = sum sims
   print total
