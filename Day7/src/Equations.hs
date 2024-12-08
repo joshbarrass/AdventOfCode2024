a2ae922708b53d916130824933839ff8e94a09b4
Joshua Barrass <josh.barrass.work@gmail.com>
2024-12-08 22:34:17 +0000

    Day 7 Part 2
    Annoyingly slow, since the complexity of checking an equation goes
    from O(2^n) to O(3^n), and I can't use bitwise operators anymore.
    
    Could potentially be sped up by checking the value of the equation as
    it's evaluated, and immediately rejecting it if the accumulator
    exceeds the test value.

--- Day7/src/Equations.hs
+++ Day7/src/Equations.hs
@@ -9,29 +9,43 @@ module Equations (
 import Data.Bits
 import Data.List
 
-data Operator = Add | Mul deriving (Show, Eq)
+data Operator = Add | Mul | Cat deriving (Show, Eq)
 type Equation = ([Integer], [Operator])
 
 getOperator :: Integer -> Operator
 getOperator 0 = Add
 getOperator 1 = Mul
+getOperator 2 = Cat
 
 evaluateOperator :: Operator -> Integer -> Integer -> Integer
 evaluateOperator Add a b = a + b
 evaluateOperator Mul a b = a * b
+evaluateOperator Cat a b = read (show a ++ show b)
+
+base :: Integer
+base = 3
+
+-- get least significant digit in base
+-- args:
+--  * base
+--  * number
+-- returns: (remainder, digit)
+getLSDinBase :: Integer -> Integer -> (Integer, Integer)
+getLSDinBase base number = number `divMod` base
 
 getOperators :: Integer -> Integer -> [Operator]
 getOperators 0 _ = []
 getOperators n i
   | (toInteger . length) ys == n = ys
-  | otherwise = getOperator (1 .&. i) : ys
-  where ys = getOperators (n-1) (i `shiftR` 1)
+  | otherwise = getOperator d : ys
+  where (r, d) = getLSDinBase base i
+        ys = getOperators (n-1) r
 
 evaluateEquation :: Equation -> Integer
 evaluateEquation (n:ns, os) = foldl' (\acc (op, b) -> evaluateOperator op acc b) n (zip os ns)
 
 maxi :: Integer -> Integer
-maxi = (2^)
+maxi = (base^)
 
 equationCanBeValid :: [Integer] -> Integer -> Bool
 equationCanBeValid ns testval = any ((== testval) . evaluateEquation) [(ns, getOperators n i) | i <- init [0..(maxi n)]]
